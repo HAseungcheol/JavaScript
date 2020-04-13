76dad16d5996037a86718c589623627e66b39c8d
leadon <ha2896@naver.com>
2020-04-13 12:50:40 +0900

    2020-04-13

--- javaScript/src/com/yedam/io/chat/ClientExample.java
+++ javaScript/src/com/yedam/io/chat/ClientExample.java
@@ -1,4 +1,4 @@
-package com.yedam.network;
+package com.yedam.io.chat;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,113 +8,109 @@ import java.net.Socket;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ClientExample extends Application {
 	Socket socket;
 
-	void displayText(String text) {
-		txtDisplay.appendText(text + "\n");
-	}
-
-	// startClient()
 	void startClient() {
 		Thread thread = new Thread() {
+
 			@Override
 			public void run() {
-				socket = new Socket();
 				try {
-					socket.connect(new InetSocketAddress("192.168.0.18", 5001));
+					socket = new Socket();
+					socket.connect(new InetSocketAddress("192.168.0.69", 5001));
 					Platform.runLater(() -> {
 						displayText("[연결 완료: " + socket.getRemoteSocketAddress() + "]");
 						btnConn.setText("stop");
 						btnSend.setDisable(false);
 					});
-				} catch (IOException e) {
-					e.printStackTrace();
+				} catch (Exception e) {
 					Platform.runLater(() -> displayText("[서버 통신 안됨]"));
 					if (!socket.isClosed()) {
 						stopClient();
 					}
+					return;
 				}
+				receive();
 			}
 		};
 		thread.start();
-	}
 
-	// stopClient()
+	}// end of startClient()
+
 	void stopClient() {
-		Platform.runLater(() -> {
-			displayText("[연결 끊음]");
-			btnConn.setText("start");
-			btnSend.setDisable(true);
-			if (socket != null && !socket.isClosed()) {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+		try {
 
-	// receive()
+			Platform.runLater(() -> {
+				displayText("[연결 끊음]");
+				btnConn.setText("start");
+				btnSend.setDisable(true);
+			});
+			if (socket != null && !socket.isClosed()) {
+				socket.close();
+			}
+		} catch (IOException e) {
+		}
+
+	}// end of stopClient()
+
 	void receive() {
 		while (true) {
-			byte[] buf = new byte[100];
 			try {
-				InputStream is = socket.getInputStream();
-				int readByte = is.read(buf);
-				if (readByte == -1) {
+				byte[] byteArr = new byte[100];
+				InputStream inputStream = socket.getInputStream();
+
+				int readByteCount = inputStream.read(byteArr);
+				if (readByteCount == -1) {
 					throw new IOException();
 				}
-				String data = new String(buf, 0, readByte, "UTF-8");
-				Platform.runLater(() -> {
-					displayText("[받기 완료]" + data);
-				});
-			} catch (IOException e) {
-				e.printStackTrace();
-				Platform.runLater(() -> {
-					displayText("[서버 통신 안됨]");
-				});
+				String data = new String(byteArr, 0, readByteCount, "UTF-8");
+				Platform.runLater(() -> displayText("[받기 완료]" + data));
+			} catch (Exception e) {
+				Platform.runLater(() -> displayText("[서버 통신 안됨]"));
 				stopClient();
 				break;
 			}
 		}
-	}
+	}// end of receive()
 
-	// send()
 	void send(String data) {
 		Thread thread = new Thread() {
+
 			@Override
 			public void run() {
 				try {
-					byte[] buf = data.getBytes("UTF-8");
-					OutputStream os = socket.getOutputStream();
-					os.write(buf);
-					os.flush();
-					Platform.runLater(() -> {
-						displayText("[보내기 완료]");
-					});
-				} catch (IOException e) {
-					e.printStackTrace();
-					Platform.runLater(() -> {
-						displayText("[서버 통신 안됨]");
-						stopClient();
-					});
+					byte[] byteArr = data.getBytes("UTF-8");
+					OutputStream outputStream = socket.getOutputStream();
+					outputStream.write(byteArr);
+					outputStream.flush();
+					Platform.runLater(() -> displayText("[보내기 완료]"));
+
+				} catch (Exception e) {
+					Platform.runLater(() -> displayText("[서버 통신 안됨]"));
+					stopClient();
 				}
 			}
 		};
 		thread.start();
+
+	}// end of send()
+
+	void displayText(String text) {
+		txtDisplay.appendText(text + "\n");
 	}
 
-	// start() => UI 만드는 메소드.
 	TextArea txtDisplay;
 	TextField txtInput;
 	Button btnConn, btnSend;
@@ -136,33 +132,50 @@ public class ClientExample extends Application {
 
 		btnConn = new Button("start");
 		btnConn.setPrefSize(60, 30);
-		btnConn.setOnAction(event -> {
-			if (btnConn.getText().equals("start")) {
-				 startClient();
-			} else if (btnConn.getText().equals("stop")) {
-				 stopClient();
-			}
+		btnConn.setOnAction(e -> {
+			if (btnConn.getText().equals("start"))
+				startClient();
+			else if (btnConn.getText().equals("stop"))
+				stopClient();
 		});
 
 		btnSend = new Button("send");
 		btnSend.setPrefSize(60, 30);
 		btnSend.setDisable(true);
-		btnSend.setOnAction(event -> send(txtInput.getText()));
+		btnSend.setOnAction(e -> {
+			send(txtInput.getText());
+			txtInput.setText("");
+		});
+
+		txtInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode().equals(KeyCode.ENTER)) {
+					send(txtInput.getText());
+					txtInput.clear();
+//					String text = txtInput.getText();
+//					txtDisplay.appendText(text);
+//					txtInput.clear();
+				}
+			}
+		});
 
 		bottom.setCenter(txtInput);
 		bottom.setLeft(btnConn);
 		bottom.setRight(btnSend);
-
 		root.setBottom(bottom);
 
 		Scene scene = new Scene(root);
-
+		scene.getStylesheets().add(getClass().getResource("app.css").toString());
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Client");
+		primaryStage.setOnCloseRequest(event -> stopClient());
 		primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
